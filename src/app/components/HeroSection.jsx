71bf442e52d5530093dd1a1beaed008e4f87e8d7
tondekawere <tondekawere@gmail.com>
2024-01-29 12:09:35 +0200

    download true

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -41,13 +41,13 @@ const HeroSection = () => {
           <div>
             <Link
               href="/#contact"
-              download={true}
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-cyan-900 to-slate-500 hover:bg-slate-200 text-white"
             >
               Hire Me
             </Link>
             <Link
-              href="https://www.canva.com/design/DAF6_J27pD8/KixRqKqTlcUEv92ca-bieQ/edit?utm_content=DAF6_J27pD8&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+              download={true}
+              href="/Tonderai cv.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-cyan-900 to-slate-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
